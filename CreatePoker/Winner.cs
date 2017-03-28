e5a3ea925e3b764d471441737f18265a1a612ad6
Jax Goller <jaxgoller@gmail.com>
2017-03-28 10:32:17 -0600

    adding winning arrays

--- CreatePoker/Winner.cs
+++ CreatePoker/Winner.cs
@@ -11,6 +11,15 @@ namespace CreatePoker
         Hand Player0 = new Hand();
         Hand Player1 = new Hand();
         River river = new River();
+        Winner[] P0 = new Winner[7];
+        Winner[] P1 = new Winner[7];
         //Add river to both player's hands
+        public Winner()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+
+            }
+        }
     }
 }
