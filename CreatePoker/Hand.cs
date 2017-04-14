1b3d39070d585d10f7593169fc76601b4ca42365
Jax Goller <jaxgoller@gmail.com>
2017-04-13 21:28:37 -0600

    adding 3 of a kind

--- CreatePoker/Hand.cs
+++ CreatePoker/Hand.cs
@@ -35,13 +35,18 @@ namespace CreatePoker
             }
             else
             {
-                if (Has2Pair() == true)
+                if (Has2Pair() == true && Has3Pair() == false)
                 {
                     score = 2;
                     Console.Write("Has 2 Pair");
                 }
                 else
                 {
+                    if (Has3Pair() == true)
+                    {
+                        score = 3;
+                        Console.Write("Has 3 Pair");
+                    }
                     score = 0;
                     Console.Write("no pairs detected");
                 }
@@ -87,6 +92,44 @@ namespace CreatePoker
                 }
             }
             if (pair1==true && pair2 == true)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool Has3Pair()
+        {
+            bool pair1 = false;
+            bool pair2 = false;
+            bool pair3 = false;
+            int num1 = -1;
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = i + 1; j < 7; j++)
+                {
+                    //whats going on here
+                    if (cards[i].Num == cards[j].Num && cards[i].Num != num1)
+                    {
+                        if (num1 == -1)
+                        {
+                            pair1 = true;
+                            num1 = 0;
+                        }
+                        else
+                        {
+                            if (num1 == 0)
+                            {
+                                pair2 = true;
+                            }
+                            else
+                            {
+                                pair3 = true;
+                            }
+                        }
+                    }
+                }
+            }
+            if (pair1 == true && pair2 == true && pair3 == true)
             {
                 return true;
             }
